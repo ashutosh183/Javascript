219727805795c8e3e99a1be0d33d042a4f834e91
ashutosh183 <ashutoshkumarpandey183@gmail.com>
2023-08-20 21:18:12 +0530

    learnt strings

---
+++ Strings/string.js
@@ -0,0 +1,16 @@
+console.log("Learning Strings!!!")
+
+let name = "Ashutosh"
+console.log(name)
+console.log("Length of my name is"+" "+name.length)
+
+let b1 = "Ashutosh"
+let b2 = "Virat"
+
+let sentence = `${b1} is fan of ${b2}`
+console.log(sentence)
+
+console.log("Escape Sequences")
+
+let fruit = 'banana\'s'
+console.log(fruit)
